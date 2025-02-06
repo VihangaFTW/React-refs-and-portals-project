7e73ffd42951eba23ce4720aeeb89790434b886b
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-02-06 14:34:16 +1100

    Modal shows score upon win

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -2,11 +2,13 @@ import { useImperativeHandle, useRef } from "react";
 
 export default function ResultModal({ ref,onReset, targetTime, remainingTime }) {
   const dialog = useRef();
-  const result = remainingTime <= 0 ? "lost" : "won";
+  const userLost = remainingTime <= 0 
+  const score = Math.round((1-remainingTime/(targetTime*1000))*100)
   useImperativeHandle(ref, () => ({ open: () => dialog.current.showModal() }));
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}!</h2>
+      {userLost && <h2>You Lost!</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime} seconds</strong>
       </p>
