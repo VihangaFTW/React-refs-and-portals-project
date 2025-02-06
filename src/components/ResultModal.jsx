812c79e35cdc76385398d8ed0efe26c29884a518
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-02-06 14:31:06 +1100

    Modal shows end game state dynamically with remaining time

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,8 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
+export default function ResultModal({ ref,onReset, targetTime, remainingTime }) {
   const dialog = useRef();
+  const result = remainingTime <= 0 ? "lost" : "won";
   useImperativeHandle(ref, () => ({ open: () => dialog.current.showModal() }));
   return (
     <dialog ref={dialog} className="result-modal">
@@ -10,9 +11,10 @@ export default function ResultModal({ ref, result, targetTime }) {
         The target time was <strong>{targetTime} seconds</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left</strong>
+        You stopped the timer with
+        <strong> {(remainingTime / 1000).toFixed(2)} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,33 +1,46 @@
-import { useState, useRef } from "react";
-
+import {useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({ targetTime, title }) {
-  const [timerExpired, setTimerExpired] = useState(false);
-  const [timerStarted, setTimerStarted] = useState(false);
+  const remainingTime = useRef(targetTime * 1000);
 
-  const timer = useRef();
+  // ref to hold the interval
+  const intervalId = useRef();
+  // ref to open up dialog after game ends
   const modal = useRef();
 
-  function handleStart() {
-    // time should be in milliseconds for this function
-    // setTimeout expects a function as first argument
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-      modal.current.open();
-    }, targetTime * 1000);
+  //keep track of game state so that the UI can re render appropriately
+  const [timerStarted, setTimerStarted] = useState(false);
 
+  function handleStart() {
+    // every 10ms, deduct remaining time by 10ms effectively giving the remaining time
+    intervalId.current = setInterval(() => {
+      remainingTime.current -= 10;
+      if (remainingTime.current <= 0){
+        handleStop();
+      }
+    }, 10);
     setTimerStarted(true);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(intervalId.current);
+    modal.current.open();
     setTimerStarted(false);
+  }
+
+  function onFormClose(){
+    remainingTime.current = targetTime*1000;
   }
 
   return (
     <>
-      <ResultModal ref={modal} result="lost" targetTime={targetTime} />
+      <ResultModal
+        onReset = {onFormClose}
+        ref={modal}
+        targetTime={targetTime}
+        remainingTime={remainingTime.current}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
@@ -35,11 +48,12 @@ export default function TimerChallenge({ targetTime, title }) {
         </p>
         <p>
           <button onClick={!timerStarted ? handleStart : handleStop}>
-            {timerStarted && !timerExpired ? "Stop" : "Start"} Challenge
+            {timerStarted ? "Stop " : "Start "}
+            Challenge
           </button>
         </p>
-        <p className={timerStarted && !timerExpired ? "active" : undefined}>
-          {timerStarted && !timerExpired ? "Time is running" : "Timer inactive"}
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Time is running" : "Timer inactive"}
         </p>
       </section>
     </>
